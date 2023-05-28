5892f53f218098ebf6c92729d54dff8f8cb972d3
Abhishek <upendranathsingh00@gmail.com>
2023-05-29 02:09:37 +0530

    Fifth day of Sde Sheet solving

---
+++ ARRAY/Majority_Element.cpp
@@ -0,0 +1,104 @@
+// here we have to return that element whose frequency is greater than n/2 where n is the size of the array
+
+// #include<bits/stdc++.h>
+// using namespace std ;
+// #define long long int int;
+// signed main(){
+// int t;
+// cin>>t;
+// while(t--){
+// int n ;
+// cin>>n;
+// vector<int>v(n);
+// bool flag =true;
+// for(auto &it:v) cin>>it;
+// for(int i=0;i<n;i++){
+//     int x=v[i];
+//     int count=0;
+//     for(int j=i;j<n;j++){
+//         if(v[j]==x) count++;
+//     }
+//     if(count>(v.size()/2)) {
+//     cout<<x<<endl;
+//       flag=false;
+//       break;
+//     }
+// }
+// if(flag) cout<<"NO SUCH VALUE"<<endl;
+// }
+// return 0;
+// }
+
+// Brute Force 
+// tc=o(n^2)
+// sc=o(1)
+
+// here we are dealing with counting of frequency so how can we forget to use map 
+// let's go
+
+// #include<bits/stdc++.h>
+// using namespace std ;
+// #define long long int int;
+// signed main(){
+// int t;
+// cin>>t;
+// while(t--){
+// int n ;
+// cin>>n;
+// vector<int>v(n);
+// for(auto &it:v) cin>>it;
+// map<int,int>mp;
+// bool flag=true;
+// for(auto &it:v) mp[it]++;
+// for(auto &it:mp){
+//     if(it.second>(v.size()/2)){
+//         cout<<it.first<<endl;
+//         flag=false;
+//     }
+// }
+// if(flag) cout<<"NO SUCH VALUE"<<endl;
+// }
+// return 0;
+// }
+
+// tc =o(nlogn) filling the map + o(n) iterating the map
+// sc=o(n) as in worst case each element of array can be unique
+
+// Optimal Approach 
+// Intuition : We will find the probable element which can have frequency greater than n/2 
+
+// We will use +- concept as all elements which will have frequency less than n/2 there will be their counterpart //present in array to cancel it out 
+
+#include<bits/stdc++.h>
+using namespace std ;
+#define long long int int;
+signed main(){
+int t;
+cin>>t;
+while(t--){
+int n ;
+cin>>n;
+vector<int>v(n);
+for(auto &it:v) cin>>it;
+int count=0,ele=-1;
+for(int i=0;i<n;i++){
+  if(count==0){
+    ele=v[i];
+    count++;
+  }
+  if(v[i]==ele) count++;
+  else count--;
+}
+int probable_answer=ele;
+//Verification
+int count1=0;
+for(int i=0;i<n;i++){
+if(v[i]==ele) count1++;
+}
+if(count1>(v.size()/2))cout<<ele<<endl;
+else cout<<"NO SUCH ELEMENT EXISTS"<<endl;
+}
+return 0;
+}
+
+
---
+++ ARRAY/Sort_An_Array_Of_One's_Zero's_And_Two's.cpp
@@ -0,0 +1,99 @@
+// #include<bits/stdc++.h>
+// using namespace std ;
+// #define long long int int;
+// signed main(){
+// int t;
+// cin>>t;
+// while(t--){
+// int n ;
+// cin>>n;
+// vector<int>v(n);
+// for(auto &it:v) cin>>it;
+// sort(begin(v),end(v));
+// for(auto &it:v) cout<<it<<" ";
+// cout<<endl;
+// }
+// return 0;
+// }
+
+/// Above discussed is bruteforce wherein we are sorting that usually takes nlogn time complexity 
+// Space complexity in case of merge sort being o(n) i.e of being auxillary array or may be in quick sort we won't be requiring any extra space 
+
+// #include<bits/stdc++.h>
+// using namespace std ;
+// #define long long int int;
+// signed main(){
+// int t;
+// cin>>t;
+// while(t--){
+// int n ;
+// cin>>n;
+// vector<int>v(n);
+// for(auto &it:v) cin>>it;
+// int cnt_0=0,cnt_1=0,cnt_2=0;
+// for(auto &it:v){
+//     if(it==0) cnt_0++;
+//     else if(it==1) cnt_1++;
+//     else cnt_2++;
+// }
+// for(int i=0;i<cnt_0;i++){
+//     v[i]=0;
+// }
+// for(int i=cnt_0;i<cnt_0+cnt_1;i++){
+//     v[i]=1;
+// }
+// for(int i=cnt_0+cnt_1;i<n;i++){
+//     v[i]=2;
+// }
+// for(auto &it:v) cout<<it<<" ";
+// cout<<endl;
+// }
+// return 0;
+// }
+
+// tc =o(n) for storing count of each element and o(n) for replacing values 
+// sc = o(1);
+
+// Optimised Solution
+//======> DUTCH NATIONAL FLAG ALGORITHM <=======
+// INTUITION: ARRAY INDEX RANGES FROM  0 TO N-1
+// WE WILL BE HAVING THREE POINTERS LOW MID AND HIGH
+// TARGET : O- LOW-1 // ALL ZEROES
+// TARGET : LOW- MID-1 // ALL ONES
+// TARGET: MID-HIGH// UNSORTED 
+// TARGET :HIGH+1- N-1 // ALL TWOS
+// INITIALLY MID AND LOW WILL POINT TO 0 AND HIGH POINT TO N-1
+// DECISION MAKING : 
+// V[MID]==0 THEN SWAP(V[LOW],V[MID]) LOW++, MID++;
+// V[MID]==1  MID++;
+// V[MID]==2 SWAP(V[MID],V[HIGH]) HIGH--;
+
+#include<bits/stdc++.h>
+using namespace std ;
+#define long long int int;
+signed main(){
+int t;
+cin>>t;
+while(t--){
+int n ;
+cin>>n;
+vector<int>v(n);
+for(auto &it:v) cin>>it;
+int low=0,mid=0,high=n-1;
+while(mid<=high){
+if(v[mid]==0){
+    swap(v[low],v[mid]);
+    low++;
+    mid++;
+}
+else if(v[mid]==1) mid++;
+else{
+    swap(v[high],v[mid]);
+    high--;
+}
+}
+for(auto &it:v) cout<<it<<" ";
+cout<<endl;
+}
+return 0;
+}
